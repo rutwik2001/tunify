3adec7cf5fd544992519550186f0e55e41cd147a
Pasumarthi Rutwik <rutwikpasumarthi.01@gmail.com>
2022-04-24 19:59:23 +0530

    resolved port error

--- app.js
+++ app.js
@@ -494,6 +494,7 @@ app.post("/search", async (req, res) => {
 // });
 
 
-app.listen(3000, function() {
+const port = process.env.PORT || 3000
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
